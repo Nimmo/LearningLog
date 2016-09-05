50445d2bb6e1ebc0303382ff2be9958ffadba7ed
Graeme Nimmo <graeme@nimmog.co.uk>
2016-09-05 23:07:54 +0100

    Completed Version 1 of the log writer and viewer.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
 import getpass
 import time
 import json
@@ -9,8 +11,8 @@ import os.path
 log_file = ""
 learning_log = []
 
-class LearningConversationPupil(BoxLayout):
 
+class LearningLog(FloatLayout):
     learning_intention = ObjectProperty()
     success_yes = ObjectProperty()
     success_part = ObjectProperty()
@@ -37,9 +39,14 @@ class LearningConversationPupil(BoxLayout):
 
         json.dump(learning_log, open(log_file, "w"))
 
+    def show_learning_log(self):
+        self.clear_widgets()
+        self.add_widget(LearningLog())
 
-class LearningConversations(App):
-    pass
+
+class LearningLogApp(App):
+    def build(self):
+        return LearningLog()
 
 
 def get_session():
@@ -51,9 +58,53 @@ def get_session():
         session = str(year - 1) + "-" + str(year)
     return session
 
+class TableHeader(Label):
+    pass
+
+
+class LearningRecord(Label):
+    pass
+
+
+class MyGrid(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.fetch_data_from_database()
+        self.display_scores()
+
+    def fetch_data_from_database(self):
+        self.data = learning_log
+
+    def display_scores(self):
+        self.clear_widgets()
+
+        row = self.create_header()
+        for item in row:
+            self.add_widget(item)
+
+        for i in range(len(self.data)):
+            row = self.add_record_to_table(i)
+            for item in row:
+                self.add_widget(item)
+
+    def create_header(self):
+        first_column = TableHeader(text="Date")
+        second_column = TableHeader(text="Learning Intention")
+        third_column = TableHeader(text="Successful?")
+        fourth_column = TableHeader(text="Achievement in Lesson")
+        fifth_column = TableHeader(text="Next Steps")
+        return [first_column, second_column, third_column, fourth_column, fifth_column]
+
+    def add_record_to_table(self, i):
+        first_column = LearningRecord(text=self.data[i]['date'])
+        second_column = LearningRecord(text=self.data[i]['learning_intention'])
+        third_column = LearningRecord(text=self.data[i]['success'])
+        fourth_column = LearningRecord(text=self.data[i]['lesson_achievement'])
+        fifth_column = LearningRecord(text=self.data[i]['next steps'])
+        return [first_column, second_column, third_column, fourth_column, fifth_column]
+
 if __name__ == '__main__':
-
-
     username = getpass.getuser()
     session = get_session()
     log_file = username + " " + session + " learning log.json"
@@ -61,4 +112,5 @@ if __name__ == '__main__':
     if os.path.isfile(log_file):
         learning_log = json.load(open(log_file))
 
-    LearningConversations().run()
+    LearningLogApp().run()
+
